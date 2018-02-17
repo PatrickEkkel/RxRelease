c1b60009b7803d7cd3bbf725fa587edafef59b2c
Patrick Ekkel <patrick.ekkel@topicus.nl>
2018-02-17 22:07:39 +0100

    statetypes worden nu geladen via profiletypes en capabilities

--- backend/rxrelease/rxbackend/views/hostviews.py
+++ backend/rxrelease/rxbackend/views/hostviews.py
@@ -4,7 +4,8 @@ from ..serializers import HostSerializer
 from ..models import Host
 from ..models import StateType
 from ..models import State
-
+from ..models import Capability
+from ..models import ProfileType
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -18,17 +19,24 @@ logger.addHandler(ch)
 class CreateView(generics.ListCreateAPIView):
     """This class defines the create behavior of our rest api."""
     queryset = Host.objects.all()
+    #profiletypes_queryset = ProfileType.objects.all()
     serializer_class = HostSerializer
     def perform_create(self, serializer):
+
+        # instead of taking all the statetypes, get the profiletype from the host,
+        # and than get its capebilities which contains all relevant stateypes
        statetype_set = StateType.objects.all()
        host = serializer.save()
-       for statetype in statetype_set.iterator():
-           state = State()
-           state.statetype = statetype
-           state.name = statetype.name
-           state.installed = False
-           state.host = host
-           state.save()
+       capabilities = host.profileType.capabilities
+       for capability in capabilities.iterator():
+           for statetype in capability.statetypes.iterator():
+            #for statetype in statetype_set.iterator():
+            state = State()
+            state.statetype = statetype
+            state.name = statetype.name
+            state.installed = False
+            state.host = host
+            state.save()
 class DetailsView(generics.RetrieveUpdateDestroyAPIView):
     """This class handles the http GET, PUT and DELETE requests."""
     queryset = Host.objects.all()
